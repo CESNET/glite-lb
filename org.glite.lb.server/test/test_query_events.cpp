717e55461aa895e35be581691eeb83907694f07f
Aleš Křenek <ljocha@ics.muni.cz>
2004-09-30 08:24:10 +0000

    Fix the test build

--- org.glite.lb.server/test/test_query_events.cpp
+++ org.glite.lb.server/test/test_query_events.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <fstream>
 
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/CompilerOutputter.h>
@@ -13,6 +13,8 @@
 
 using namespace std;
 
+static const char *test_dir;
+
 class QueryEventsTest: public CppUnit::TestFixture
 {
 	CPPUNIT_TEST_SUITE(QueryEventsTest);
@@ -21,8 +23,10 @@ class QueryEventsTest: public CppUnit::TestFixture
 
 private:
 	edg_wll_Context	ctx;
-	vector<pair<string,vector<string>>>	expQueries;
-	int					queryIdx;
+	
+	ifstream	qry_file;
+
+	vector<pair<string,vector<string> > >	queries;
 
 public:
 	void oneJob();
@@ -43,27 +47,63 @@ void QueryEventsTest::oneJob()
 
 	job[0].attr = EDG_WLL_QUERY_ATTR_JOBID;
 	job[0].op = EDG_WLL_QUERY_OP_EQUAL ;
-	edg_wlc_JobIdParse("https://fake.server/fake_job",&job[0].value.j);
+	edg_wlc_JobIdParse("https://lhun.ics.muni.cz:4850/WrCEKje9QTXFiSOZuPMLtw",
+		&job[0].value.j);
 	job[1].attr = EDG_WLL_QUERY_ATTR_UNDEF;
+	
+	string file(test_dir);
+	file += "/oneJob.qry";
+	qry_file.open(file.c_str());
+	
+	while (!qry_file.eof()) {
+		string	query,line;
+		vector<string>	rows;
 
-	expQueries.clear();
-	/*
-	 *	XXX: ...
-	 */
-	expQueries.push_back();
+		getline(qry_file,query);
+		cout << "read: " << query <<endl;
+		rows.clear();
+
+		while (!qry_file.eof()) {
+			getline(qry_file,line);
+			if (line == "") break;
+	
+			rows.push_back(line);
+		}
+		rows.push_back("END");
+		queries.push_back(pair<string,vector<string> >(query,rows));
+	}
+
+	qry_file.close();
+
 	CPPUNIT_ASSERT(!edg_wll_QueryEventsServer(ctx,1,jobs,NULL,&events));
 }
 
-int QueryEventsTest::ExecStmt(const char *, edg_wll_Stmt *)
+int QueryEventsTest::ExecStmt(const char *qry, edg_wll_Stmt *stmt_out)
 {
-	return 0;
+	vector<pair<string,vector<string> > >::iterator	stmt = queries.begin();
+
+	for (; stmt != queries.end(); stmt++) {
+		const char	*q = stmt->first.c_str();
+
+		/* XXX: there some spaces at the end of qry */
+		if (!strncmp(q,qry,strlen(q))) break;
+	}
+
+	if (stmt == queries.end()) {
+		cerr << "query not found" << endl;
+		CPPUNIT_ASSERT(0);
+	}
+	vector<string>::iterator	*rows = new vector<string>::iterator(stmt->second.begin());
+
+	*stmt_out = (edg_wll_Stmt) rows;
+	return stmt->second.size()-1;
 }
 
 extern "C" {
 
 int edg_wll_ExecStmt(edg_wll_Context ctx,char *qry,edg_wll_Stmt *stmt)
 {
-	cout << qry << endl;
+	cout << "edg_wll_ExecStmt: " << qry << endl;
 
 	class QueryEventsTest *tst = (class QueryEventsTest *)(ctx->mysql);
 	return tst->ExecStmt(qry, stmt);
@@ -71,7 +111,16 @@ int edg_wll_ExecStmt(edg_wll_Context ctx,char *qry,edg_wll_Stmt *stmt)
 
 int edg_wll_FetchRow(edg_wll_Stmt stmt, char **cols)
 {
-	return 0;
+	vector<string>::iterator	*rows = (vector<string>::iterator *) stmt;
+	char	*row,*p,i=0;
+
+	if (**rows == "END") return 0;
+	row = strdup((*rows)->c_str());
+	(*rows)++;
+	for (p = strtok(row,"\t"); p; p = strtok(NULL,"\t"))
+		cols[i++] = strdup(p);
+
+	return i;
 }
 
 void edg_wll_FreeStmt(edg_wll_Stmt *) {}
@@ -95,6 +144,8 @@ int main (int ac,const char *av[])
 {
 	CppUnit::Test *suite = CppUnit::TestFactoryRegistry::getRegistry().makeTest();
 	CppUnit::TextUi::TestRunner runner;
+
+	test_dir = ac >= 2 ? av[1] : "../test";
 
 	runner.addTest(suite);
 	return runner.run() ? 0 : 1;
