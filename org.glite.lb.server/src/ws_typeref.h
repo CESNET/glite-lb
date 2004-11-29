ca75fcfc1fe9fc0ec67fe299216011d8402ce74b
Zdeněk Salvet <salvet@ics.muni.cz>
2004-11-29 19:06:42 +0000

    Correct edg_wll_StatusToSoap() prototype.

--- org.glite.lb.server/src/ws_typeref.h
+++ org.glite.lb.server/src/ws_typeref.h
@@ -8,7 +8,7 @@ extern "C" {
 extern void edg_wll_JobStatCodeToSoap(edg_wll_JobStatCode, enum edgwll__JobStatCode *);
 extern void edg_wll_SoapToJobStatCode(enum edgwll__JobStatCode, edg_wll_JobStatCode *);
 
-extern void edg_wll_StatusToSoap(struct soap *, edg_wll_JobStat const *, struct edgwll__JobStat *);
+extern void edg_wll_StatusToSoap(struct soap *, edg_wll_JobStat const *, struct edgwll__JobStat **);
 extern void edg_wll_SoapToStatus(struct soap *, struct edgwll__JobStat const *, edg_wll_JobStat **);
 
 extern void edg_wll_SoapToJobStatFlags(const struct edgwll__JobStatFlags *, int *);
