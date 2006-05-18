d0c115ddedbfbb616319e23a6302953d047c769c
Michal Voců <michal@ruk.cuni.cz>
2006-05-18 14:07:55 +0000

    * more bones to skeleton

--- org.glite.lb.common/src/perftest_common.sh
+++ org.glite.lb.common/src/perftest_common.sh
@@ -4,7 +4,16 @@ run_test()
 {
     for file in small_job big_job small_dag big_dag
     do
-	perftest_logjobs --file=$file --dst=$dest --num=$num_jobs
+	perftest_logjobs --test=$test_name --file=$file --dst=$dest --num=$num_jobs
     done
 }
 
+print_results()
+{
+    total=`echo "scale=9; $PERFTEST_END_TIMESTAMP - $PERFTEST_BEGIN_TIMESTAMP" | bc`
+    echo -e "Total time for PERFTEST_NUM_JOBS: \t$total"
+    echo -e -n "Event throughput: \t"
+    echo "scale=9; $PERFTEST_NUM_JOBS * $PERFTEST_JOB_SIZE / $total" |bc
+    echo -e -n "Job throughput: \t"
+    echo "scale=9; $PERFTEST_NUM_JOBS / $total" |bc
+}
