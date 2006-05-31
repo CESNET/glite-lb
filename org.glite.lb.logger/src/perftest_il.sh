74e29b36eb157e2166e4a588c8cf023d53ae3a1a
Michal Voců <michal@ruk.cuni.cz>
2006-05-31 15:47:26 +0000

    * first serious IL test

--- org.glite.lb.logger/src/perftest_il.sh
+++ org.glite.lb.logger/src/perftest_il.sh
@@ -1,35 +1,65 @@
+#!/bin/bash
 
-    dst=il
+numjobs=$1
 
-# i)1)
+# XXX - there must be better way to find stage
+STAGEDIR=/home/michal/shared/egee/jra1-head/stage
+. $STAGEDIR/sbin/perftest_common.sh
 
-    glite_lb_interlogd_perf_noparse_nosend
-    run_test()
+DEBUG=${DEBUG:-0}
+# CONSUMER_ARGS=
+# PERFTEST_COMPONENT=
+# COMPONENT_ARGS=
+#LOGJOBS_ARGS="" 
 
-    glite_lb_interlogd_perf_nosync_nosend
-    run_test()
+check_test_files || exit 1
 
-    glite_lb_interlogd_perf_norecover_nosend
-    run_test()
+PERFTEST_CONSUMER=$STAGEDIR/bin/glite-lb-interlogd-perf-empty
+CONSUMER_ARGS="-d -v"
 
-    glite_lb_interlogd_perf_nosend
-    run_test()
+echo -e "\tsmall_job \t big_job \t small_dag \t big_dag"
+run_test il $numjobs
+j=0
+while [[ $j -lt 4 ]]
+do
+    echo -e -n "\t ${PERFTEST_EV_THROUGHPUT[$j]}"	
+    j=$((j+1))
+done
+echo ""
 
-# ii)1)
 
-glite_lb_bkserverd_perf_empty
-
-    glite_lb_interlogd_perf_noparse
-    run_test()
-
-    glite_lb_interlogd_perf_nosync
-    run_test()
-    
-    glite_lb_interlogd_perf_norecover
-    run_test()
-
-    glite_lb_interlogd_perf_lazy
-    run_test()
-
-    glite_lb_interlogd_perf
-    run_test()
+#
+#    dst=il
+#
+## i)1)
+#
+#    glite_lb_interlogd_perf_noparse --nosend
+#    run_test()
+#
+#    glite_lb_interlogd_perf_nosync --nosend
+#    run_test()
+#
+#    glite_lb_interlogd_perf_norecover --nosend
+#    run_test()
+#
+#    glite_lb_interlogd_perf --nosend
+#    run_test()
+#
+## ii)1)
+#
+#glite_lb_bkserverd_perf_empty
+#
+#    glite_lb_interlogd_perf_noparse
+#    run_test()
+#
+#    glite_lb_interlogd_perf_nosync
+#    run_test()
+#    
+#    glite_lb_interlogd_perf_norecover
+#    run_test()
+#
+#    glite_lb_interlogd_perf_lazy
+#    run_test()
+#
+#    glite_lb_interlogd_perf
+#    run_test()
