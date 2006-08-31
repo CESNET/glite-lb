cd7b24f72da9b24f21d30b2776d36b696e24fdff
Aleš Křenek <ljocha@ics.muni.cz>
2006-08-31 14:23:45 +0000

    util to extract fields from ULM

---
+++ org.glite.lb.client/examples/ulmfields.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl -n
+
+BEGIN{
+	@pf = split /,/,shift;
+};
+
+next if /^\s*$/;
+
+@F = split / /;
+
+undef $prev;
+undef %f;
+
+for $f (@F) {
+	if ($f =~ /^[.A-Z_]+="/) {
+#		print $prev,"\n" if $prev;
+		@P = split /=/,$prev,2;
+		$f{$P[0]} = $P[1];
+		$prev = $f;
+	}
+	else { $prev .= $f; }
+}
+
+# print $prev,"\n";
+@P = split /=/,$prev,2;
+$f{$P[0]} = $P[1];
+
+for $f (@pf) {
+	print "$f=$f{$f}\n";
+}
+print "\n";
