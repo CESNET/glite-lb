f23a1c8b43b90174a68c879885d46dcb4cdb285c
Michal Voců <michal@ruk.cuni.cz>
2007-03-07 09:34:30 +0000

    * some work

--- org.glite.lb.logger/src-nt/HTTPTransport.cpp
+++ org.glite.lb.logger/src-nt/HTTPTransport.cpp
@@ -1,6 +1,7 @@
 #include "HTTPTransport.H"
 
 #include <iostream>
+#include <string.h>
 
 
 HTTPTransport::Factory HTTPTransport::theFactory;
@@ -8,25 +9,49 @@ HTTPTransport::Factory HTTPTransport::theFactory;
 
 HTTPTransport::~HTTPTransport()
 {
+	if(body) free(body);
 }
 
 
 void
 HTTPTransport::onReady()
 {
-	char buffer[256];
-	int len;
+	switch(state) {
+	case NONE:
+		state = IN_REQUEST;
+		pos = buffer;
 
-	len = conn->read(buffer, sizeof(buffer));
-	if(len < 0) {
-		std::cout << "error on receive - closing connection" << std::endl;
-	} else if ( len > 0) {
-		std::cout.write(buffer, len);
-		std::cout.flush();
-		ThreadPool::theThreadPool.queueWorkRead(this);
-	} else {
-		std::cout << "no more data" << std::endl;
+	case IN_REQUEST:
+	case IN_HEADERS:
+		len = conn->read(buffer, sizeof(buffer));
+		if(len < 0) {
+			// error during request
+		} else if(len == 0) {
+			// other side closed connection
+		} else {
+			char *cr;
+
+			// parse buffer, look for CRLF
+			cr = memchr(buffer, '\r', len);
+			if(cr) {
+				if((cr < buffer + len - 1) &&
+				   (cr[1] == '\n')) {
+					// found CRLF
+					
+				} else 
+					cr = buffer + len;
+			}
+			// append everything up to the cr into appropriate variable
+			if(state == IN_REQUEST) 
+				request.append(buffer, cr - buffer);
+			else
+				headers.append(buffer, cr - buffer);
+		}
+
+	case IN_BODY:
+		break;
 	}
+
 }
 
 
@@ -40,3 +65,5 @@ void
 HTTPTransport::onError()
 {
 }
+
+
