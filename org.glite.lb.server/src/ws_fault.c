13303e534bfe9f3f01a0b1eb52b81dafb08b2602
František Dvořák <valtri@civ.zcu.cz>
2007-03-21 12:27:42 +0000

    Small code cleanup (due to gSoap compatibility changes).

--- org.glite.lb.server/src/ws_fault.c
+++ org.glite.lb.server/src/ws_fault.c
@@ -2,21 +2,18 @@
 
 #include "glite/lb/context-int.h"
 #include "soap_version.h"
+#include "glite/security/glite_gscompat.h"
 
 #include "bk_ws_H.h"
 #include "bk_ws_Stub.h"
 
 
 #if GSOAP_VERSION >= 20709
-  #define GFTYPE lbt__genericFault
   #define GFITEM reason
   #define GFNUM SOAP_TYPE_lbt__genericFault
-  #define GFREASON(SOAP) ((SOAP)->fault->SOAP_ENV__Reason ? (SOAP)->fault->SOAP_ENV__Reason->SOAP_ENV__Text : "(no reason)")
 #else
-  #define GFTYPE _genericFault
   #define GFITEM lbe__genericFault
   #define GFNUM SOAP_TYPE__genericFault
-  #define GFREASON(SOAP) ((SOAP)->fault->SOAP_ENV__Reason)
 #endif
 
 
@@ -24,8 +21,11 @@ void edg_wll_ErrToFault(const edg_wll_Context ctx,struct soap *soap)
 {
 	char	*et,*ed;
 	struct SOAP_ENV__Detail	*detail = soap_malloc(soap,sizeof *detail);
-	struct GFTYPE *f = soap_malloc(soap,sizeof *f);
-
+#if GSOAP_VERSION >= 20709
+	struct lbt__genericFault *f = soap_malloc(soap,sizeof *f);
+#else
+	struct _genericFault *f = soap_malloc(soap,sizeof *f);
+#endif
 
 	f->GFITEM = soap_malloc(soap,sizeof *f->GFITEM);
 	memset(f->GFITEM, 0, sizeof(*f->GFITEM));
@@ -81,7 +81,7 @@ void edg_wll_FaultToErr(const struct soap *soap,edg_wll_Context ctx)
 		char	*s;
 
 		asprintf(&s,"SOAP: %s", soap->version == 2 ?
-			GFREASON(soap) : soap->fault->faultstring);
+			GLITE_SECURITY_GSOAP_REASON(soap) : soap->fault->faultstring);
 		edg_wll_SetError(ctx,EINVAL,s);
 		free(s);
 	}
