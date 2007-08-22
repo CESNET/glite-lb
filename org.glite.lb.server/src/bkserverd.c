526d946515cfc7687275652096e97dc060ded734
Zdeněk Salvet <salvet@ics.muni.cz>
2007-08-22 12:54:01 +0000

    Better debugging message.

--- org.glite.lb.server/src/bkserverd.c
+++ org.glite.lb.server/src/bkserverd.c
@@ -755,7 +755,7 @@ int bk_handle_connection(int conn, struct timeval *timeout, void *data)
 	case 1:
 */
 		if ( !edg_wll_gss_acquire_cred_gsi(server_cert, server_key, &newcred, NULL, &gss_code) ) {
-			dprintf(("[%d] reloading credentials\n", getpid()));
+			dprintf(("[%d] reloading credentials successful\n", getpid()));
 			gss_release_cred(&min_stat, &mycred);
 			mycred = newcred;
 		} else { dprintf(("[%d] reloading credentials failed, using old ones\n", getpid())); }
