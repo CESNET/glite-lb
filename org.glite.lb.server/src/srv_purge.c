1de237cc477e1431d34fecd694ba54bce6f3afb8
Zdeněk Salvet <salvet@ics.muni.cz>
2007-10-09 20:32:13 +0000

    Avoid returning without sending answer to client in edg_wll_PurgeServer().

--- org.glite.lb.server/src/srv_purge.c
+++ org.glite.lb.server/src/srv_purge.c
@@ -217,7 +217,7 @@ int edg_wll_PurgeServer(edg_wll_Context ctx,const edg_wll_PurgeRequest *request)
 
 	if ( (request->flags & EDG_WLL_PURGE_SERVER_DUMP) && 
 		 ((dumpfile = edg_wll_CreateTmpPurgeFile(ctx, &tmpfname)) == -1 ) )
-		return edg_wll_Error(ctx, NULL, NULL);
+		goto abort;
 
 	/* 
 	should be changed so that only purged events are sent to whole-server dumps
@@ -234,7 +234,7 @@ int edg_wll_PurgeServer(edg_wll_Context ctx,const edg_wll_PurgeRequest *request)
 		{
 			if ( request->flags & EDG_WLL_PURGE_SERVER_DUMP )
 				unlink(tmpfname);
-			return edg_wll_Error(ctx, NULL, NULL);
+			goto abort;
 		}
 	}
 	*/
@@ -328,10 +328,9 @@ int edg_wll_PurgeServer(edg_wll_Context ctx,const edg_wll_PurgeRequest *request)
 			}
 		}
 		edg_wll_FreeStmt(&s);
-abort:
-                // just for escaping from nested cycles
-	        ;       /* prevent compiler to complain */
 	}
+
+abort:
 
 	if (parse && !edg_wll_Error(ctx,NULL,NULL))
 	{
