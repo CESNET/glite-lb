df02e4bd99c29acb341f02569bf75c4cba10feeb
Aleš Křenek <ljocha@ics.muni.cz>
2008-02-27 14:07:04 +0000

    *** empty log message ***

---
+++ org.glite.lb.client/src/notify_supp.h
@@ -0,0 +1,10 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+char * parse_fields(const char *,void **);
+void print_fields(void **,const edg_wll_NotifId,edg_wll_JobStat const *);
+
+#ifdef __cplusplus
+}
+#endif
