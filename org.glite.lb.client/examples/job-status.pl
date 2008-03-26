1917f3416e289d0f1c501ddd6de195fc14fee859
Aleš Křenek <ljocha@ics.muni.cz>
2008-03-26 16:43:58 +0000

    comments

--- org.glite.lb.client/examples/job-status.pl
+++ org.glite.lb.client/examples/job-status.pl
@@ -3,6 +3,8 @@
 use SOAP::Lite;
 use Data::Dumper;
 
+# see user-jobs.pl for comments on this magic
+
 $ENV{HTTPS_CA_DIR}='/etc/grid-security/certificates';
 $ENV{HTTPS_VERSION}='3';
 
@@ -15,24 +17,31 @@ $host = $1; $port = $2;
 $server = $host . ':' . ($port + 3);
 
 $c = SOAP::Lite
-	-> proxy($server) -> uri('http://glite.org/wsdl/services/lb');
+	-> proxy($server)
+	-> uri('http://glite.org/wsdl/services/lb');
 
 service $c 'http://egee.cesnet.cz/cms/export/sites/egee/en/WSDL/HEAD/LB.wsdl';
 ns $c 'http://glite.org/wsdl/elements/lb';
+
+# we need another namespace explicitely here
 $c->serializer->register_ns('http://glite.org/wsdl/types/lb','lbtype');
 
 on_fault $c sub { print Dumper($_[1]->fault); $fault = 1; };
 
+# construct the request, see SOAP::Data docs for details
 $req = SOAP::Data->value(
 		SOAP::Data->name(jobid=>$job),
+# here the other namespace is used
 		SOAP::Data->name(flags=>'')->type('lbtype:jobFlags')
 	);
 
+# call the WS op
 $resp = JobStatus $c $req;
 $body = $resp->body();
 
 # print Dumper $resp->body();
 
+# pick something reasonable from the response to print
 unless ($fault) {
 	@fields = qw/state jobtype owner networkServer doneCode exitCode childrenNum/;
 	$stat = $resp->body()->{JobStatusResponse}->{stat};
