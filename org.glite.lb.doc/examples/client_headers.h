c6b27ce2c198d860496ad52d1bc15066590e4eeb
Michal Voců <michal@ruk.cuni.cz>
2008-03-26 15:13:18 +0000

    example code moved from lb.client

---
+++ org.glite.lb.doc/examples/client_headers.h
@@ -0,0 +1,4 @@
+#include "glite/lb/context.h"
+#include "glite/lb/xml_conversions.h"
+#include "glite/lb/consumer.h"
+
---
+++ org.glite.lb.doc/examples/example1_code.c
@@ -0,0 +1,31 @@
+edg_wll_Context     ctx;    
+edg_wll_JobStat    *statesOut = NULL;
+edg_wll_QueryRec    jc[2];
+
+edg_wll_InitContext(&ctx);
+
+jc[0].attr = EDG_WLL_QUERY_ATTR_JOBID;
+jc[0].op = EDG_WLL_QUERY_OP_EQUAL;
+if ( edg_wlc_JobIdParse(
+	     "https://lhun.ics.muni.cz:9000/OirOgeWh_F9sfMZjnIPYhQ",
+	     &jc[0].value.j) )
+{
+	edg_wll_FreeContext(ctx);
+	exit(1);
+}
+jc[1].attr = EDG_WLL_QUERY_ATTR_UNDEF;
+if (edg_wll_QueryJobs(ctx, jc, 0, NULL, &statesOut)) {
+	char    *err_text,*err_desc;
+	
+	edg_wll_Error(ctx,&err_text,&err_desc);
+	fprintf(stderr,"QueryJobs: %s (%s)\n",err_text,err_desc);
+	free(err_text);
+	free(err_desc);
+}
+else {
+	print_jobs(statesOut);	/* process the returned data */
+	edg_wll_FreeStatus(statesOut);
+	free(statesOut);
+}
+edg_wlc_JobIdFree(jc[0].value.j);
+edg_wll_FreeContext(ctx);
