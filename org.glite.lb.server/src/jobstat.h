30a18bea89eabeea646502809e97479a3b363953
Aleš Křenek <ljocha@ics.muni.cz>
2008-04-09 15:38:35 +0000

    #include lbu/db.h where it's really needed

--- org.glite.lb.server/src/jobstat.h
+++ org.glite.lb.server/src/jobstat.h
@@ -5,6 +5,7 @@
 
 #include "glite/lb/jobstat.h"
 #include "glite/lb/intjobstat.h"
+#include "glite/lbu/db.h"
 
 int edg_wll_JobStatusServer(edg_wll_Context, glite_jobid_const_t, int, edg_wll_JobStat *);
 
