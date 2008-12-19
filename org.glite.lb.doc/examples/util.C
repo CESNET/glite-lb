bcef7056544cacbd5ed1606b08db72548e17f12f
Michal Voců <michal@ruk.cuni.cz>
2008-12-19 17:04:23 +0000

    c++ api work

--- org.glite.lb.doc/examples/util.C
+++ org.glite.lb.doc/examples/util.C
@@ -11,17 +11,17 @@ void
 dumpEvent(Event *event)
 {
 // list of attribute names and types
-	typedef std::vector<std::pair<Event::Attr, Event::AttrType>> AttrListType;
+	typedef vector<pair<Event::Attr, Event::AttrType>> AttrListType;
 	
 	cout << "Event name: " << event->name() << endl;
-	AttrListType attr_list = event->getAttrs();
+	AttrListType attr_list = event->getAttrs(); //* \label{l:getattrs}
 	for(AttrListType::iterator i = attr_list.begin();
 	    i != attr_list.end();
 	    i++) {
-		Event::Attr attr = attr_list[i].first();
+		Event::Attr attr = attr_list[i].first;
 		
-		cout << Event::getAttrName(attr) << " = ";
-		switch(attr_list[i].second()) {
+		cout << Event::getAttrName(attr) << " = "; 
+		switch(attr_list[i].second) {
 		case Event::INT_T: 
 		case Event::PORT_T:
 		case Event::LOGSRC_T:
@@ -60,6 +60,84 @@ dumpEvent(Event *event)
 /*status*/
 void dumpState(JobStatus *status) 
 {
-	
+	typedef vector<pair<JobStatus:Attr, JobStatus::AttrType>> AttrListType;
+
+	cout << "Job status: " << status->name << endl;
+
+	AttrListType attr_list = status->getAttrs(); //* \label{l:jgetattrs}
+	for(AttrListType::iterator i = attr_list.begin();
+	    i != attr_list.end();
+	    i++) {
+		JobStatus::Attr attr = attr_list[i].first;
+		cout << JobStatus::getAttrName(attr) << " = ";
+		switch(attr_list[i].second) {
+
+		case INT_T:
+			cout << status->getValInt(attr) << endl;
+			break;
+			
+		case STRING_T: 
+			cout << status->getValInt(attr) << endl;
+			break;
+
+		case TIMEVAL_T:
+			cout << status->getValTime(attr).tv_sec << endl;
+			break;
+
+		case BOOL_T:
+			cout << status->getValBool(attr).tv_sec << endl;
+			break;
+
+		case JOBID_T:
+			cout << status->getValJobid(attr).toString() << endl;
+			break;
+			
+		case INTLIST_T:
+			vector<int> list = status->getValIntList(attr);
+			for(vector<int>::iterator i = list.begin();
+			    i != list.end();
+			    i++) {
+				cout << list[i] << " ";
+			}
+			cout << endl;
+			break;
+			
+		case STRLIST_T:
+			vector<string> list = status->getValStringList(attr);
+			for(vector<string>::iterator i = list.begin();
+			    i != list.end();
+			    i++) {
+				cout << list[i] << " ";
+			}
+			cout << endl;
+			break;
+
+		case TAGLIST_T: /**< List of user tags. */
+			vector<pair<string, string>> list = status->getValTagList(attr);
+			for(vector<pair<string,string>>::iterator i = list.begin();
+			    i != list.end();
+			    i++) {
+				cout << list[i].first << "=" << list[i].second << " ";
+			}
+			cout << endl;
+			break;
+
+		case STSLIST_T:  /**< List of states. */
+			vector<JobStatus> list = status->getValJobStatusList(attr);
+			for(vector<JobStatus>::iterator i = list.begin();
+			    i != list.end();
+			    i++) {
+				// recursion
+				dumpState(&list[i]);
+			}
+			cout << endl;
+			break;
+			
+		default:
+			cout << "attribute type not supported" << endl;
+			break;
+
+		}
+	}
 }
 /*end status*/
