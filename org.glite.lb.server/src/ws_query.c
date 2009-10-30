1a8e10457a8fab126b96bfab70905436913a7b46
Aleš Křenek <ljocha@ics.muni.cz>
2009-10-30 15:14:35 +0000

    missing parameter name

--- org.glite.lb.server/src/ws_query.c
+++ org.glite.lb.server/src/ws_query.c
@@ -410,7 +410,7 @@ SOAP_FMAC5 int SOAP_FMAC6 __lb__NotifDrop(
 }
 
 SOAP_FMAC5 int SOAP_FMAC6 __lb4agu__GetActivityStatus(
-	struct soap*,
+	struct soap* soap,
 	struct _lb4ague__GetActivityStatusRequest *in,
 	struct _lb4ague__GetActivityStatusResponse *out)
 {
