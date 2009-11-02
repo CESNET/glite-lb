f4be6620f74d8f7a0401e5b358afa2d77a599290
Jan Pospíšil <honik@ntc.zcu.cz>
2009-11-02 20:56:36 +0000

    lb4agu - work in progress - added skeleton for GetActivityInfo

--- org.glite.lb.server/src/ws_query.c
+++ org.glite.lb.server/src/ws_query.c
@@ -409,14 +409,6 @@ SOAP_FMAC5 int SOAP_FMAC6 __lb__NotifDrop(
 	return SOAP_OK;
 }
 
-SOAP_FMAC5 int SOAP_FMAC6 __lb4agu__GetActivityStatus(
-	struct soap* soap,
-	struct _lb4ague__GetActivityStatusRequest *in,
-	struct _lb4ague__GetActivityStatusResponse *out)
-{
-	/* TODO */
-	return SOAP_OK;
-}
 
 static void freeQueryRecsExt(edg_wll_QueryRec **qr) {
 	int i, j;
