7754c03e9eb180ee6fe2d536496c7f71840aa083
Michal Voců <michal@ruk.cuni.cz>
2011-09-26 11:39:13 +0000

    Torque specific changes

--- org.glite.lb.state-machine/src/process_event_pbs.c
+++ org.glite.lb.state-machine/src/process_event_pbs.c
@@ -85,8 +85,8 @@ int processEvent_PBS(intJobStat *js, edg_wll_Event *e, int ev_seq, int strict, c
 				if (!js->pub.pbs_queue)
 					js->pub.pbs_queue = strdup(e->PBSQueued.queue);
 				assert(!strcmp(js->pub.pbs_queue, e->PBSQueued.queue));
-				rep_cond(js->pub.pbs_owner,e->PBSQueued.owner);
-				rep_cond(js->pub.pbs_name,e->PBSQueued.name);
+				/* rep_cond(js->pub.pbs_owner,e->PBSQueued.owner);
+				rep_cond(js->pub.pbs_name,e->PBSQueued.name); */
 			}
 			break;
 		case EDG_WLL_EVENT_PBSMATCH:
