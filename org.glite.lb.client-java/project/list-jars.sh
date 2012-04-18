c34b8a0dfb319446870e467f13151cc04b3d96c6
František Dvořák <valtri@civ.zcu.cz>
2012-04-18 17:29:07 +0000

    One more jar dependency (build on SL5/6).

--- org.glite.lb.client-java/project/list-jars.sh
+++ org.glite.lb.client-java/project/list-jars.sh
@@ -4,7 +4,7 @@
 #
 
 PREFIXES="${@:-'/usr/lib /usr/share/java'}"
-LIST="activation ant-apache-bcel ant-apache-bsf ant-apache-log4j ant-apache-oro ant-apache-regexp ant-apache-resolver ant-apache-xalan2 ant-commons-logging ant-commons-net ant-javamail ant-jdepend ant-jmf ant-jsch ant-junit ant-nodeps ant-swing ant-trax axis axis-jaxrpc axis-saaj jaxrpc saaj commons-codec commons-discovery commons-lang commons-logging-adapters commons-logging-api commons-logging el-api gettext gnome-java-bridge gnumail gnumail-providers inetlib jobid-api-java jsp-api libintl servlet-api wsdl4j xercesImpl xml-apis"
+LIST="activation ant-apache-bcel ant-apache-bsf ant-apache-log4j ant-apache-oro ant-apache-regexp ant-apache-resolver ant-apache-xalan2 ant-commons-logging ant-commons-net ant-javamail ant-jdepend ant-jmf ant-jsch ant-junit ant-nodeps ant-swing ant-trax axis axis-jaxrpc axis-saaj jaxrpc saaj commons-codec commons-discovery commons-lang commons-logging-adapters commons-logging-api commons-logging el-api gettext gnome-java-bridge gnumail gnumail-providers inetlib jobid-api-java jsp-api libintl log4j servlet-api wsdl4j xercesImpl xml-apis"
 
 CP=""
 for prefix in $PREFIXES; do
