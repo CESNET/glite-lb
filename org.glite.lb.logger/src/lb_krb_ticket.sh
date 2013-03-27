8c73d91b7695ea0fa42ffb494931a352b97cd46c
Michal Voců <michal@ruk.cuni.cz>
2013-03-27 18:05:11 +0000

    more general version taking defaults from system configuration

--- org.glite.lb.logger/src/lb_krb_ticket.sh
+++ org.glite.lb.logger/src/lb_krb_ticket.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-mkdir -p /var/glite
+[ -f /etc/default/glite-lb ] && . /etc/default/glite-lb
 
-ticket=/var/glite/krb5cc_lb
+if [ "x$GLITE_GSS_MECH" != "xkrb5" ]
+then
+    exit
+fi
+
+GLITE_USER=${GLITE_USER:-'glite'}
+GLITE_HOME=`getent passwd ${GLITE_USER} | cut -d: -f6`
+
+# KRB5_KTNAME here is different than KRB5_KTNAME in startup scripts,
+# here we use system wide keytab to obtain tickets for interlogd, 
+# which uses them as a client connecting to LB server
+# (keytab in startup scripts is used by logd acting as a server)
+KRB5_KTNAME="/etc/krb5.keytab"
+
+ticket=${GLITE_HOME}/krb5cc_lb
+KRB5CCNAME="FILE:$ticket"
+
+[ -d $GLITE_HOME ] || mkdir -p $GLITE_HOME
 
 KTUTIL="/usr/sbin/ktutil"
 HOSTNAME="/bin/hostname"
@@ -12,6 +29,6 @@ PRINCIPAL=`${KTUTIL} list 2>/dev/null | grep $($HOSTNAME -f) | grep host | awk '
 
 KRB5CCNAME="FILE:${ticket}0" ${KINIT} -k $PRINCIPAL
 
-chown glite:glite ${ticket}0
+chown $GLITE_USER:$GLITE_GROUP ${ticket}0
 
 mv ${ticket}0 $ticket
