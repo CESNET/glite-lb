7d731817b7af7e367cbe4e19b269942ea9222c20
František Dvořák <valtri@civ.zcu.cz>
2013-09-03 16:22:59 +0200

    Output errors to syslog instead of stdout/stderr during ticket renewing (CESNET RT# 75962).

--- org.glite.lb.logger/project/debian.glite-lb-logger.cron.d
+++ org.glite.lb.logger/project/debian.glite-lb-logger.cron.d
@@ -1,1 +1,1 @@
-0 */2 * * * root if [ -e /usr/share/glite-lb-logger/lb_krb_ticket.sh ]; then /usr/share/glite-lb-logger/lb_krb_ticket.sh; fi
+0 */2 * * * root if [ -e /usr/share/glite-lb-logger/lb_krb_ticket.sh ]; then /usr/share/glite-lb-logger/lb_krb_ticket.sh 2>&1 | logger -t glite-lb-logger-krb-ticket -p user.error; fi
